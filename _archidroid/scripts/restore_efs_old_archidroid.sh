32b97d5b543555d6339a9184ce5ef19e79fdf651
mrtur <mrturmrtur@gmail.com>
2013-08-19 07:26:10 +0200

    Fix problem

--- _archidroid/scripts/restore_efs_old_archidroid.sh
+++ _archidroid/scripts/restore_efs_old_archidroid.sh
@@ -4,6 +4,6 @@ mount /dev/block/mmcblk0p3 /efs
 busybox tar -zxvf /data/media/0/ArchiDroid/Backups/efs_OLD.tar.gz -C /
 umount /efs
 DATE1=`date`
-echo "efs_old restore $DATE1" >>  /data/media/0/ArchiDroid/Backups/efs.txt
+echo "efs_OLD restore $DATE1" >>  /data/media/0/ArchiDroid/Backups/efs.txt
 fi
 exit 0
